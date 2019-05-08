2013e4f9196236fc66958426d640ca1fd0fd2edd
J. Eduardo V. Morango <edvmorango@gmail.com>
2019-05-07 22:16:32 -0300

    chore: sqs consuming from sns

--- createQueue.sh
+++
@@ -1,1 +0,0 @@
-aws --endpoint-url http://localhost:9324 sqs create-queue --queue-name user_message_queue
--- createTopics.sh
+++
@@ -1,5 +0,0 @@
-aws sns --endpoint-url http://localhost:9911 create-topic --name user_message_topic
-
-
-#aws sns --endpoint-url http://localhost:9911 subscribe --topic-arn arn:aws:sns:us-east-1:123456789012:user_message_topic
-#--protocol email --notification-endpoint my-email@example.com
